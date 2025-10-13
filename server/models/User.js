56db8f2505b631001bb2a1217d7fab190ec13d64
Jabir7006 <www.roman1249@gmail.com>
2025-10-13 14:38:57 +0600

    feat: add User model with schema definition for MongoDB

---
+++ server/models/User.js
@@ -0,0 +1,37 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Full Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please enter a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user", "manager"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;
