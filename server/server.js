4341266762327b36e2c57a9d3038e0139b222bcd
Jabir7006 <www.roman1249@gmail.com>
2025-10-13 11:07:45 +0600

    feat: add mongoose dependency and connect to database on server start

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
+  await connectDB();
 });
