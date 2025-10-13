6fe686f98f812705bf18a38df872d10c32e54190
Jabir7006 <www.roman1249@gmail.com>
2025-10-13 16:18:42 +0600

    feat: add Brand and Category models with schema definitions for MongoDB

---
+++ server/models/Brand.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const brandSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Brand Title is required"],
+    },
+    slug: {
+      type: String,
+      required: [true, "Brand Slug is required"],
+      unique: true,
+      lowercase: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Brand = mongoose.model("Brand", brandSchema);
+
+export default Brand;
