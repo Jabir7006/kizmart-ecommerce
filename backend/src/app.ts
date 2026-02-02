c4a29b44679779af47615cbf2a4430563c1e02f7
Jabir7006 <www.roman1249@gmail.com>
2026-02-02 15:30:54 +0600

    feat: Add body parsing middleware.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import { APP_ORIGIN } from './constants/env.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: APP_ORIGIN,
