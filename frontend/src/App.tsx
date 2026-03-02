50deed38506fa6b4ec9b1c3a8554c907514a526b
Jabir7006 <www.roman1249@gmail.com>
2026-03-02 22:24:30 +0600

    feat: Initialize frontend with React Router, Shadcn UI, and path aliases.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "@/pages/Home";
+
+const Signin = lazy(() => import("@/pages/auth/Signin"));
+const Signup = lazy(() => import("@/pages/auth/Signup"));
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
